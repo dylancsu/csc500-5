d233185d42e56a21aeb958bb8c843125fb4628b8
dylancsu <dylan.dean@csuglobal.edu>
2024-08-18 17:33:23 -0600

    Do part 1 maybe

--- part1.py
+++ part1.py
@@ -1,1 +1,9 @@
 
+years = int(input("Enter the number of years:\n"))
+inches = 0
+
+for y in range(0,years):
+    for m in range(0,12):
+        inches += int(input(f"Enter the number of inches of rain in month {m} of year {y}:\n"))
+
+print(f"In {12*years} months, there was {inches} inches of rainfall, averaging {inches/(12*years):.2f} inches per month")
